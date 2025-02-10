f69c1117a4bbd2fe2482261b8b0fe332920ef1c7
ManuelParrado <113886755+ManuelParrado@users.noreply.github.com>
2025-02-10 19:03:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 
-# Cargar el modelo, junto con DictVectorizer
-with open("models/dt-energy-consumption-model.pck", "rb") as f:
-    dv, dt_model = pickle.load(f)
-
 with open("models/svm-energy-consumption-model.pck", "rb") as f:
-    _, svm_model = pickle.load(f)  # No cargamos dv nuevamente
+    dv, svm_model = pickle.load(f)
 
 # Título de la aplicación
 st.title("Predicción de Consumo de Energía")
@@ -45,12 +42,9 @@ if st.button("Predecir"):
 
     # Transformar los datos del usuario usando DictVectorizer
     X_input = dv.transform([input_data])
-
-    # Realizar predicciones
-    dt_prediction = dt_model.predict(X_input)[0]
-    svm_prediction = svm_model.predict(X_input)[0]
+    
+    svm_prediction = svm_model.predict(X_input)
 
     # Mostrar resultado
     st.subheader("Resultados de Predicción:")
-    st.write(f"**Decision Tree Prediction:** {dt_prediction}")
     st.write(f"**SVM Prediction:** {svm_prediction}")
