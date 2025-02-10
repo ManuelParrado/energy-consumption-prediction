da91deee9b47297a52b73234e62f3b4c177a9dfd
ManuelParrado <113886755+ManuelParrado@users.noreply.github.com>
2025-02-10 20:30:59 +0100

    Delete energy-consumption-complete-project directory

--- energy-consumption-complete-project/models-api/energy_consumption_predict_service.py
+++
@@ -1,9 +0,0 @@
-def predict_single(customer, dv, model):
-    try:
-        x = dv.transform([customer])
-        y_pred = model.predict(x)  # Predicción de consumo energético
-
-        # Devolvemos la predicción
-        return y_pred[0]
-    except Exception as e:
-        raise ValueError(f"Error during SVM prediction: {e}")
